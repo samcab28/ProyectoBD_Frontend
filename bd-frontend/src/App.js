6e0e40084142a5f5711e1227cf0a02ad2badfb11
samcab28 <samircabrera2528@gmail.com>
2024-05-27 15:22:33 -0600

    ruta homescreen

--- bd-frontend/src/App.js
+++ bd-frontend/src/App.js
@@ -2,10 +2,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// Importaciones del homescreen
-import HomeScreen from './homeScreen/HomeScreen';
-import Login from './homeScreen/Login';
-import About from './homeScreen/About';
+// Importacion del homescreen
+import RouteHomeScreen from "./router/RouteHomeScreen";
 
 // Importación de rutas separadas
 import RouteAdmin from './router/RouteAdmin';
@@ -18,14 +16,10 @@ function App() {
         <div>
             <BrowserRouter>
                 <Routes>
+                    {/*path original*/}
+
                     {/* Path inicial */}
-                    <Route path='/' element={<HomeScreen />} />
-
-                    {/* Paths de homescreen */}
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/about' element={<About />} />
-
-                    {/* Paths de initial */}
+                    <Route path='/*' element={<RouteHomeScreen />} />
                     <Route path='/veterinario/*' element={<RouteVeterinario />} />
                     <Route path='/gerente/*' element={<RouteGerente />} />
                     <Route path='/cliente/*' element={<RouteCliente />} />
---
+++ bd-frontend/src/router/RouteHomeScreen.js
@@ -0,0 +1,21 @@
+// src/router/RouteAdmin.js
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+// Importación de componentes de administración
+import HomeScreen from "../homeScreen/HomeScreen";
+import About from "../homeScreen/About";
+import Login from "../homeScreen/Login";
+
+const RouteAdmin = () => {
+    return (
+        <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/login' element={<Login />} />
+            {/* Agrega más rutas de administración aquí según sea necesario */}
+        </Routes>
+    );
+};
+
+export default RouteAdmin;
