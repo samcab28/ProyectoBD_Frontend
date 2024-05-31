024521f6f89935aec3d7692d46356b1e6bee9a85
Bonko24 <eazofp2106@gmail.com>
2024-05-31 14:16:45 -0600

    q

---
+++ bd-frontend/src/CRUD_Folder/crudProducto/CreateProducto.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+function CreateProducto () {
+    const [nombreProducto, setNombreProducto] = useState('');
+    const [precioProducto, setNombreProducto] = useState('');
+    const [descripcionProducto, setNombreProducto] = useState('');
+    const [cantidadDisponible, setNombreProducto] = useState('');
+    const [nombreSucursal, setNombreProducto] = useState('');
+    const [nombreProducto, setNombreProducto] = useState('');
+    const [nombreProducto, setNombreProducto] = useState('');
+    const [nombreProducto, setNombreProducto] = useState('');
+}
+
+export default CreateProducto
