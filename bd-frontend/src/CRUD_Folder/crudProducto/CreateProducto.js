1b77d336e0580b2bf28eeb7a1bcae19e6085ed35
Bonko24 <eazofp2106@gmail.com>
2024-05-31 14:29:15 -0600

    q2

--- bd-frontend/src/CRUD_Folder/crudProducto/CreateProducto.js
+++ bd-frontend/src/CRUD_Folder/crudProducto/CreateProducto.js
@@ -2,13 +2,19 @@ import React, { useState, useEffect } from "react";
 
 function CreateProducto () {
     const [nombreProducto, setNombreProducto] = useState('');
-    const [precioProducto, setNombreProducto] = useState('');
-    const [descripcionProducto, setNombreProducto] = useState('');
-    const [cantidadDisponible, setNombreProducto] = useState('');
-    const [nombreSucursal, setNombreProducto] = useState('');
-    const [nombreProducto, setNombreProducto] = useState('');
-    const [nombreProducto, setNombreProducto] = useState('');
-    const [nombreProducto, setNombreProducto] = useState('');
+    const [precioProducto, setPrecioProducto] = useState('');
+    const [descripcionProducto, setDescripcionProducto] = useState('');
+    const [cantidadDisponible, setCantidadDisponible] = useState('');
+    const [nombreSucursal, setNombreSucursal] = useState('');
+    const [tipoProducto, setTipoProducto] = useState('');
+    const [nombreMarcaPro, setNombreMarcaPro] = useState('');
+    const [url, setUrl] = useState('');
+
+    const []
+
+    useEffect(() => {
+
+    }, []);
 }
 
 export default CreateProducto
