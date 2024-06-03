9de5ed09d82590cd2f09486a24f5b26a763f0e24
samcab28 <samircabrera2528@gmail.com>
2024-06-03 17:14:15 -0600

    update seleccion Sucursal

--- bd-frontend/src/pantallas/pantallaCliente/ProductoCliente.js
+++ bd-frontend/src/pantallas/pantallaCliente/ProductoCliente.js
@@ -1,15 +1,17 @@
 // src/PaginasCliente/ProductoCliente.js
 import React, { useContext, useEffect, useState } from 'react';
-import {  useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../../context/UserContext';
 import '../../Styles/PageContainer.css';
 import fondoVet from '../../Imagenes/FondoVet.jpg';
 import NavCliente from "./NavCliente";
-import ProductImage from '../../Imagenes/ProductImage.js'; // Importa el nuevo componente
+import ProductImage from '../../Imagenes/ProductImage'; // Asegúrate de que ProductImage.js existe y exporta un componente
 
 function ProductoCliente() {
     const navigate = useNavigate();
     const [products, setProducts] = useState([]);
+    const [sucursales, setSucursales] = useState([]);
+    const [selectedSucursal, setSelectedSucursal] = useState(null); // Estado para almacenar la sucursal seleccionada
     const { user } = useContext(UserContext);
 
     useEffect(() => {
@@ -20,6 +22,23 @@ function ProductoCliente() {
                 setProducts(data);
             })
             .catch(error => console.error('Error fetching productos:', error));
+    }, []);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/sucursal')
+            .then(response => response.json())
+            .then(data => {
+                console.log("Sucursales fetched:", data);
+                if (Array.isArray(data)) {
+                    setSucursales(data);
+                    if (data.length > 0) {
+                        setSelectedSucursal(data[0]); // Establecer la primera sucursal como seleccionada
+                    }
+                } else {
+                    console.error('La respuesta de sucursales no es un array:', data);
+                }
+            })
+            .catch(error => console.error('Error fetching sucursales:', error));
     }, []);
 
     const handleAddToCart = (IdProducto) => {
@@ -50,6 +69,14 @@ function ProductoCliente() {
         navigate(`/cliente/resena/${IdProducto}`);
     };
 
+    const handleSucursalChange = (e) => {
+        const selectedId = parseInt(e.target.value);
+        const selected = sucursales.find(sucursal => sucursal.IdSucursal === selectedId);
+        setSelectedSucursal(selected);
+        console.log(selected);
+    };
+
+
     return (
         <div className="home-screen">
             <header className="header">
@@ -57,19 +84,36 @@ function ProductoCliente() {
             </header>
             <NavCliente/>
             <main className="main-content">
-                <h2>Lista de Productos</h2>
+                <h1>Lista de Productos</h1>
+                <h3>Seleccione la sucursal donde desea hacer la compra</h3>
+                <form>
+                    <label>
+                        Sucursal:
+                        <select
+                            name="sucursal"
+                            value={selectedSucursal ? selectedSucursal.IdSucursal : ''}
+                            onChange={handleSucursalChange}
+                        >
+                            {sucursales.map(sucursal => (
+                                <option key={sucursal.IdSucursal} value={sucursal.IdSucursal}>
+                                    {sucursal.NombreSucursal}
+                                </option>
+                            ))}
+                        </select>
+                    </label>
+                </form>
+                <h3>Nombre de sucursal seleccionada: {selectedSucursal ? selectedSucursal.NombreSucursal : 'Ninguna'}</h3>
                 <div className="product-grid">
                     {products.map(product => (
                         <div className="product-card" key={product.IdProducto}>
-                            <ProductImage url={product.IdURL} alt={product.NombreProducto} />
+                            <ProductImage url={product.IdURL} alt={product.NombreProducto}/>
                             <div className="product-info">
                                 <p><strong>Nombre:</strong> {product.NombreProducto}</p>
                                 <p><strong>Precio:</strong> {product.PrecioProducto}</p>
                                 <p><strong>Marca:</strong> {product.NombreMarcaPro}</p>
                                 <p><strong>Disponibles:</strong> {product.CantidadDisponible}</p>
                                 <p><strong>Descripción:</strong> {product.DescripcionProducto}</p>
-                                <p><strong>URL:</strong> {product.IdURL}</p>
-                                <button style={{ marginBottom: '10px', marginRight: '10px' }} onClick={() => handleResenaGo(product.IdProducto)} className="form-button">Resena</button>
+                                <button style={{ marginBottom: '10px', marginRight: '10px' }} onClick={() => handleResenaGo(product.IdProducto)} className="form-button">Reseña</button>
                                 <button onClick={() => handleAddToCart(product.IdProducto)} className="form-button">Agregar al Carrito</button>
                             </div>
                         </div>
