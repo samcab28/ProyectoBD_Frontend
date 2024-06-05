c09c2db17f455d29644a467bb23bb6236c82fbb0
AndyBS1 <andy230505@gmail.com>
2024-06-04 23:10:59 -0600

    Crear Cita

---
+++ bd-frontend/src/pantallas/pantallaAdmin/CrearCita.js
@@ -0,0 +1,147 @@
+import React, { useState, useEffect } from 'react';
+
+function CreateCita(){
+    const [FechaCita, setFechaCita] = useState('');
+    const [Duracion, setDuracion] = useState('');
+    const [Encargado, setEncargado] = useState('');
+    const [Mascota, setMacota] = useState('');
+    const [Estado, setEstado] = useState('');
+
+    const [personas, setPersonas] = useState([]);
+    const [mascotas, setMascotas] = useState([]);
+    const [estados, setEstados] = useState([]);
+
+    useEffect(() => {
+        // Fetch personas (gerentes)
+        fetch('http://localhost:3001/persona/tipo/2') 
+            .then(response => response.json())
+            .then(data => {
+                console.log("personas fetched:", data); // Debug line
+                setPersonas(data);
+            })
+            .catch(error => console.error('Error fetching personas:', error));
+        
+        // Fetch estados (de cita)
+        fetch('http://localhost:3001/estadoCita') 
+            .then(response => response.json())
+            .then(data => {
+                console.log("personas fetched:", data); // Debug line
+                setEstados(data);
+            })
+            .catch(error => console.error('Error fetching personas:', error));
+
+        // Fetch animales
+        fetch('http://localhost:3001/mascota')
+            .then(response => response.json())
+            .then(data => {
+                console.log("productos fetched:", data); // Debug line
+                setMascotas(data);
+            })
+            .catch(error => console.error('Error fetching productos:', error));
+    }, []);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const newCita = {
+            FechaCita: FechaCita,
+            DuracionCita: Duracion,
+            IdMascota: parseInt(Mascota),
+            IdEncargado: parseInt(Encargado),
+            EstadoCita : parseInt(Estado)
+        };
+
+        fetch('http://localhost:3001/citaMedica', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newCita)
+        })
+            .then(response => {
+                if (response.ok) {
+                    alert('Cita creada exitosamente');
+                    window.location.reload(); // Recargar la página
+                } else {
+                    alert('Error al crear cita');
+                }
+            })
+            .catch(error => {
+                console.error('Error en la solicitud:', error);
+            });
+    }
+
+    return(
+        <div>
+            <h2>Crear Cita</h2>
+            <form onSubmit={handleSubmit}>
+            <label>
+                    Fecha:
+                    <input
+                        name="fechaCita"
+                        type="date"
+                        value={FechaCita}
+                        onChange={e => setFechaCita(e.target.value)}
+                    />
+                </label>
+                <br/>
+                <label>
+                    Duración:
+                    <input
+                        name="Duracion"
+                        type="text"
+                        value={Duracion}
+                        onChange={e => setDuracion(e.target.value)}
+                    />
+                </label>
+                <br/>
+                <label>
+                    Encargado:
+                    <select
+                        name="encargado"
+                        value={Encargado}
+                        onChange={e => setEncargado(e.target.value)}
+                    >
+                        <option value="">Selecciona un encargado</option>
+                        {personas.map(persona => (
+                            <option key={persona.IdPersona} value={persona.IdPersona}>{persona.NombrePersona} {persona.ApellidoPersona}</option>
+                        ))}
+                    </select>
+                </label>
+                <br/>
+                <label>
+                    Mascota:
+                    <select
+                        name="mascota"
+                        value={Mascota}
+                        onChange={e => setMacota(e.target.value)}
+                    >
+                        <option value="">Selecciona una mascota</option>
+                        {mascotas.map(mascota => (
+                            <option key={mascota.IdMascota} value={mascota.IdMascota}>{mascota.NombreMascota} </option>
+                        ))}
+                    </select>
+                </label>
+                <br/>
+                <label>
+                    Estado:
+                    <select
+                        name="estado"
+                        value={Estado}
+                        onChange={e => setEstado(e.target.value)}
+                    >
+                        <option value="">Selecciona un estado</option>
+                        {estados.map(estado => (
+                            <option key={estado.IdEstCita} value={estado.IdEstCita}>{estado.TipoEstCita} </option>
+                        ))}
+                    </select>
+                </label>
+                <br/>
+                <button type="reset">Reset data</button>
+                <button type="submit">Guardar</button>
+            </form>
+        </div>
+    );
+}
+
+export default CreateCita;
--- bd-frontend/src/pantallas/pantallaAdmin/GestionCitas.js
+++ bd-frontend/src/pantallas/pantallaAdmin/GestionCitas.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import CreateCita from './CrearCita';
 
 function GestionCitas(){
     const navigate = useNavigate(); 
@@ -8,6 +9,27 @@ function GestionCitas(){
     const handleRegresar = () => {
         navigate('/admin/citasMedica'); 
     };
+
+    function handleDelete(id){
+        fetch(`http://localhost:3001/citaMedica/${id}`, {
+            method: 'DELETE',
+        })
+        .then(response => {
+            if (response.ok) {
+                // Remove the deleted persona from the state
+                setCitas(citas.filter(cita => cita.IdAnimal !== id));
+                window.location.reload();
+            } else {
+                alert('Error deleting cita');
+            }
+        })
+        .catch(error => console.error('Error deleting cita:', error));
+
+    }
+
+    function handleMod(id){
+        console.log("implementar logica");
+    }
 
     useEffect(() => {
         fetch('http://localhost:3001/citaMedica')
@@ -19,6 +41,7 @@ function GestionCitas(){
     return (
         <div>
             <h1>Gestión de Citas Médicas</h1>
+            <CreateCita/>
             <h2>Listado de Citas Médicas</h2>
             <table>
                 <thead>
@@ -40,6 +63,11 @@ function GestionCitas(){
                         <td>{cita.NombreMascota}</td>
                         <td>{cita.Encargado}</td>
                         <td>{cita.TipoEstCita}</td>
+
+                        <td>
+                            <button onClick={() => handleDelete(cita.IdCitaMed)}>Eliminar</button>
+                            <button onClick={() => handleMod(cita.IdCitaMed)}>Modificar</button>
+                        </td>
                     </tr>
                 ))}
                 </tbody>       
