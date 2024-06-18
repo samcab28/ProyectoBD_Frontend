528825cc478e848fd3133d896f7c7f243675cfed
samcab28 <samircabrera2528@gmail.com>
2024-06-17 19:57:09 -0600

    Update InitialAdmin.js

--- bd-frontend/src/pantallas/pantallaAdmin/InitialAdmin.js
+++ bd-frontend/src/pantallas/pantallaAdmin/InitialAdmin.js
@@ -1,11 +1,23 @@
 // InitialAdmin.js
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../../Styles/PageContainer.css';
 import {Link} from "react-router-dom"; // Importa el archivo de estilos
 import NavAdmin from "./NavAdmin";
 import fondoVet from "../../Imagenes/FondoVet.jpg";
+import NotificacionHistorial from "../../seguridad/NotificacionHistorial";
 
 function InitialAdmin() {
+
+    const confirmacionLoginFallido = () => {
+        NotificacionHistorial.checkHistorialLoginMinuto()
+            .then(cantidad => console.log(cantidad))
+            .catch(error => console.error('Error:', error));
+    };
+
+    useEffect(() => {
+        confirmacionLoginFallido();
+    }, []);
+    
     return (
         <div className="home-screen">
             <header className="header">
